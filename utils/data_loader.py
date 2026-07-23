c4fb3f5df52d2d2cfda2bd873152246d8a84792d
At1b <atibshaikh2005@gmail.com>
2026-07-23 13:32:38 +0530

    Initial Commit

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,4 +1,4 @@
-import os
+""" import os
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
@@ -20,4 +20,11 @@ engine = create_engine(DATABASE_URL)
 def load_data():
     print("Loading data from the database...")
     query = "SELECT * FROM loan_data;"
-    return pd.read_sql(query, engine)
+    return pd.read_sql(query, engine) """
+    
+import pandas as pd
+import streamlit as st
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("data/raw/loan_data.csv")    
